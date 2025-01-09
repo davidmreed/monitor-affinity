bfb2f1d52ebf9db4601796da783c293869c68673
David Reed <david@ktema.org>
2025-01-09 10:42:34 -0700

    Add Nix package skeleton

--- flake.nix
+++ flake.nix
@@ -6,10 +6,34 @@
 
   outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { system = system; config.allowUnfree = true; }; in
+      let pkgs = import nixpkgs { system = system; }; in
       {
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [ rustc cargo pkg-config openssl rustfmt rust-analyzer xorg.libxcb ];
+          packages = with pkgs; [ rustc cargo clippy pkg-config openssl rustfmt rust-analyzer xorg.libxcb ];
+        };
+
+        packages.default = pkgs.rustPlatform.buildRustPackage rec {
+            pname = "monitor-affinity";
+            version = "0.1.0";
+
+            src = pkgs.fetchFromGitHub {
+              owner = "davidmreed";
+              repo = pname;
+              rev = version;
+              hash = "";
+            };
+
+            cargoHash = "";
+
+            nativeBuildInputs = with pkgs; [ xorg.libxcb ];
+            buildInputs = with pkgs; [ xorg.libxcb ];
+
+            meta = {
+                description = "";
+                homepage = "https://github.com/davidmreed/monitor-affinity";
+                license = pkgs.lib.licenses.mit;
+                maintainers = [ ];
+            };
         };
       }
     );

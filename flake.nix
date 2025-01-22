30bb0aaccfbe282af869d36dc670693fe25d9d6f
David Reed <david@ktema.org>
2025-01-21 17:38:11 -0700

    Lint and format flake

--- flake.nix
+++ flake.nix
@@ -1,39 +1,53 @@
 {
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs/nixpkgs-unstable;
-    flake-utils.url = github:numtide/flake-utils;
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { system = system; }; in
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { system = system; };
+      in
       {
+        formatter = pkgs.nixfmt-rfc-style;
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [ rustc cargo clippy pkg-config openssl rustfmt rust-analyzer xorg.libxcb ];
+          packages = with pkgs; [
+            rustc
+            cargo
+            clippy
+            pkg-config
+            openssl
+            rustfmt
+            rust-analyzer
+            xorg.libxcb
+          ];
         };
 
         packages.default = pkgs.rustPlatform.buildRustPackage rec {
-            pname = "monitor-affinity";
-            version = "0.1.1";
+          pname = "monitor-affinity";
+          version = "0.1.1";
 
-            src = pkgs.fetchFromGitHub {
-              owner = "davidmreed";
-              repo = pname;
-              rev = version;
-              hash = "sha256-kzJgrF7sTWOtzIafnpe9NaVD6e60xyCazSfHz7pGf5o=";
-            };
+          src = pkgs.fetchFromGitHub {
+            owner = "davidmreed";
+            repo = pname;
+            rev = version;
+            hash = "sha256-kzJgrF7sTWOtzIafnpe9NaVD6e60xyCazSfHz7pGf5o=";
+          };
 
-            cargoHash = "sha256-DGOAlGkPCQB8/lk4jGPOCmL1/WC6lOhFwOY161j7jUc=";
+          cargoHash = "sha256-DGOAlGkPCQB8/lk4jGPOCmL1/WC6lOhFwOY161j7jUc=";
 
-            nativeBuildInputs = with pkgs; [ xorg.libxcb ];
-            buildInputs = with pkgs; [ xorg.libxcb ];
+          nativeBuildInputs = with pkgs; [ xorg.libxcb ];
+          buildInputs = with pkgs; [ xorg.libxcb ];
 
-            meta = {
-                description = "Route bars and widgets to monitors based on criteria like \"largest\" or \"rightmost\".";
-                homepage = "https://github.com/davidmreed/monitor-affinity";
-                license = pkgs.lib.licenses.mit;
-                maintainers = [ ];
-            };
+          meta = {
+            description = "Route bars and widgets to monitors based on criteria like \"largest\" or \"rightmost\".";
+            homepage = "https://github.com/davidmreed/monitor-affinity";
+            license = pkgs.lib.licenses.mit;
+            maintainers = [ ];
+          };
         };
       }
     );
